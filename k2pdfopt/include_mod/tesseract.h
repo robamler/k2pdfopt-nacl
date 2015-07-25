70776b76c153ad26f302040b3255944b7c36a730
Robert Bamler <robert.bamler@gmail.com>
2015-07-25 23:53:26 +0200

    Add source code of k2pdfopt v2.32

---
+++ k2pdfopt/include_mod/tesseract.h
@@ -0,0 +1,25 @@
+/*
+** Willus.com's Tesseract C Wrappers
+**
+** 6-8-12
+**
+*/
+
+#ifndef           _TESSERACT_H_
+#define           _TESSERACT_H_
+
+#include <leptonica.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int tess_capi_init(char *datapath,char *language,int ocr_type,FILE *out,
+                   char *initstr,int maxlen);
+int tess_capi_get_ocr(PIX *pix,char *outstr,int maxlen,FILE *out);
+void tess_capi_end(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
